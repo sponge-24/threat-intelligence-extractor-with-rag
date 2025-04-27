f9f887121ea8c5f79ab3f2e807671eddca097a86
Sudharsanan K <121513706+sponge-24@users.noreply.github.com>
2025-04-27 18:48:45 +0530

    added .gitignore

--- pdf_extractor.py
+++ pdf_extractor.py
@@ -21,8 +21,3 @@ def convert_markdown_to_documents(md_text):
         for doc in split_docs
     ]
     return documents
-
-# pdf_path = "threat_reports/Cisco_Iranian-MuddyWater-regionally-focused-subgroups(03-10-2022).pdf"
-# md_text = extract_markdown_from_pdf(pdf_path)
-# documents = convert_markdown_to_documents(md_text)
-# print(md_text)
